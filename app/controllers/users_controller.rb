50eb34b69a22306d68e94d92b158a35a36b7b909
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-10 10:41:56 +0300

    Add action and route for user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,11 +22,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to edit_user_path(@user)
     else
       render :edit
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: [:new, :create, :edit, :update, :show]
 
 end
